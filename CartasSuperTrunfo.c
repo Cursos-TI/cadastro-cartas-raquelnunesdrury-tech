d9f1f388579917f6e619ea1f00b89186c09f869b
raquelnunesdrury-tech <raquelnunesdrury@gmail.com>
2026-03-30 18:55:49 +0000

    Nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,43 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  
+    char codigo[4];
+    int populacao;
+    float area, pib;
+    int pontosTuristicos;
+
+
+
   // Área para entrada de dados
 
+    printf("=== Cadastro de Carta ===\n");
+
+    printf("Codigo da carta (ex: A01): ");
+    scanf("%s", codigo);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (km²): ");
+    scanf("%f", &area);
+
+    printf("PIB: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontosTuristicos);
+
+
   // Área para exibição dos dados da cidade
+
+  
+    printf("\n=== Dados da Carta ===\n");
+    printf("Codigo: %s\n", codigo);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %.2f km²\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos Turisticos: %d\n", pontosTuristicos);
 
 return 0;
 } 
